8ca2aa32ac6e6924a544b9f50c52c94858c23f3e
cguardo <guardo.christgabriel@ue.edu.ph>
2021-05-14 15:22:24 +0900

    Added a new line of codes

--- SampleCode.py
+++ SampleCode.py
@@ -1,3 +1,8 @@
-print(wowers)
+print("wowers")
 print ("hello Word - vanessa" )
-print("Get Get Aww")
+print("Get Get Aww - alerie")
+print("rawr 2x - christ")
+print("dun dun dance - christ")
+
+#pighati, dalamhati, siphayo, poot, lumbay, hinagpis, kirot, sakit, lungkot
+
